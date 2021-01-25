eaeba0c7f5c011b1556efa769e537eb8a86239df
Usenmfon <usenmfonuko@gmail.com>
2021-01-25 08:54:19 +0100

    Added Nuget packages

--- src/BookListMVC/Models/ApplicationDbContext.cs
+++ src/BookListMVC/Models/ApplicationDbContext.cs
@@ -1,12 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace BookListMVC.Models
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplcationDbContext> options) : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
             
         }
 
-        public Dbset<Book> Books { get; set; }
+        public DbSet<Book> Books { get; set; }
     }
 }
